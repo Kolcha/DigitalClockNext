2b3a0802b25c0a369c498894657e245b6a623a20
Nick Korotysh <nick.korotysh@gmail.com>
2023-03-19 20:01:31 +0100

    added transparent window test code

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   Widget w;
+/*
+  w.setWindowFlag(Qt::FramelessWindowHint);
+  w.setAttribute(Qt::WA_TranslucentBackground);
+  w.showMaximized();
+*/
   w.show();
   return a.exec();
 }
