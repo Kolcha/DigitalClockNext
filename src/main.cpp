6d77897f4d9e3f85822d88b8b0bf7b929709bd56
Nick Korotysh <nick.korotysh@gmail.com>
2023-08-05 18:53:13 +0200

    added custom icon theme
    Iconset: Unicons Line
    Author: Iconscout (https://www.iconfinder.com/unicons-iconscout)
    License: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
+#include <QIcon>
+
 #include "app/application.hpp"
 
 int main(int argc, char *argv[])
 {
   Application a(argc, argv);
+
+  using namespace Qt::Literals::StringLiterals;
+  // set system icon theme as fallback
+  auto system_theme = QIcon::themeName();
+  QIcon::setThemeName(u"unicons-line"_s);
+  QIcon::setFallbackThemeName(system_theme);
+
   a.init();
-//  Widget w;
-/*
-  w.setWindowFlag(Qt::FramelessWindowHint);
-  w.setAttribute(Qt::WA_TranslucentBackground);
-  w.showMaximized();
-*/
-//  w.show();
   return a.exec();
 }
